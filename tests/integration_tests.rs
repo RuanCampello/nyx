415689c019d0d116f4568c14abb4f85a165b30ae
RuanCampello <ruancampello999@gmail.com>
2026-08-20 18:54:13 -0300

    test: cover compact forms and reformating of testing fixtures

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -55,7 +55,8 @@ fn expectation<'s>(name: &str) -> (Option<i32>, &'s str) {
         | "page_align"
         | "heap_alloc"
         | "heap_reclaim"
-        | "struct_shorthand" => Some(42),
+        | "struct_shorthand"
+        | "compact_forms" => Some(42),
         "factorial" | "const_factorial" => Some(120),
         "math" => Some(42),
         "nth_prime" => Some(229),
